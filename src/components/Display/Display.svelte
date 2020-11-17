3d043d9e5f82646251cb3325c135e6e317cb268a
Adrian Pilarczyk <adrian@useo.pl>
2020-11-17 20:08:49 +0100

    Add Display component

---
+++ src/components/Display/Display.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Pizza from "../Pizza/Pizza.svelte";
+</script>
+
+<div>
+  <Pizza />
+</div>
+
+<style>
+	div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid papayawhip;
+    padding: 1rem;
+  }
+</style>
