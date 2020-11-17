fccd416763a0896cf6d458da277b63ffff20a994
Adrian Pilarczyk <adrian@useo.pl>
2020-11-17 20:09:05 +0100

    Change show Display and Creator in App

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-	export let name: string;
+	import Display from "./components/Display/Display.svelte";
+	import Creator from "./components/Creator/Creator.svelte";
 </script>
 
+<header>
+	<h1>Let's make pizza</h1>
+</header>
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Display />
+	<Creator />
 </main>
 
 <style>
