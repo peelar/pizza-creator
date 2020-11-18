ea9362b1bc59c5705374097993b15d6f8e44fbdb
Adrian Pilarczyk <adrian@useo.pl>
2020-11-18 09:43:49 +0100

    Add topping button

--- src/components/Creator/Creator.svelte
+++ src/components/Creator/Creator.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import type { Topping } from '../../../types';
+  export let toppings: Topping[] = [];
+  $: toppingTypes = Array.from(new Set(toppings.reduce((prev, next) => [...prev, next.type], [])));
 </script>
 
 <style>
@@ -7,8 +10,14 @@
     border: 1px solid papayawhip;
     padding: 1rem;
   }
+
+  button {
+    margin: 0 0.5rem;
+    padding: 0.5rem 2rem;
+  }
 </style>
 
 <div>
   <h4>Customize</h4>
+  {#each toppingTypes as type}<button>{type}</button>{/each}
 </div>
