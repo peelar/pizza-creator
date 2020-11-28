5ca8b62e3e66f431a1c830497e905d36394bfa92
Adrian Pilarczyk <adrian@useo.pl>
2020-11-28 21:00:52 +0100

    Add grid to buttons

--- src/components/Creator/Creator.svelte
+++ src/components/Creator/Creator.svelte
@@ -8,17 +8,25 @@
 <style>
   div {
     flex: 1;
-    border: 1px solid papayawhip;
-    padding: 1rem;
   }
 
   button {
     margin: 0 0.5rem;
     padding: 0.5rem 2rem;
+    cursor: pointer;
+    text-align: center;
+  }
+
+  .grid {
+    display: grid;
+    grid-gap: 0.5rem;
+    grid-template-columns: 1fr 1fr;
   }
 </style>
 
 <div>
   <h4>Customize</h4>
-  {#each types as type}<button on:click={() => addTopping(type)}>{type}</button>{/each}
+  <div class="grid">
+    {#each types as type}<button on:click={() => addTopping(type)}>{type} ➕</button>{/each}
+  </div>
 </div>
